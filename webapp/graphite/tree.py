796ad82cc7e4a223ca6d58948ab1973058fb27f1
Stanislav Pak <stanislav@quora.com>
2015-11-02 22:16:59 +0000

    Add support for {a,b} pattern

--- webapp/graphite/tree.py
+++ webapp/graphite/tree.py
@@ -4,13 +4,22 @@ Contains implementation of OpenTSDB tree which helps browse metrics space.
 
 """
 
+import re
+
 from graphite.logger import log
+
+ANY_PATTERN = '.*'
 
 
 class OpenTSDBMetricsMeta(object):
     """Metrics tree."""
     def __init__(self, api):
         self.api = api
+
+    def _branch_current_path(self, branch):
+        path = branch['path']
+        path = '.'.join([v for _, v in sorted(path.items())])
+        return path
 
     def _branch_current_node_name(self, branch):
         return branch['path'][str(branch['depth'])]
@@ -22,15 +31,28 @@ class OpenTSDBMetricsMeta(object):
 
         """
         log.info("OpenTSDBMetricsMeta.find(%s)" % path)
-        node_names = path.split('.')
-        if node_names[-1] == '*':
+        node_names = []
+        for part in path.split('.'):
+            part = part.replace('*', ANY_PATTERN)
+            part = re.sub(
+                r'{([^{]*)}',
+                lambda x: "(%s)" % '|'.join(("^%s$" % y for y in x.groups()[0].split(','))),
+                part,
+            )
+            node_names.append(part)
+
+        if node_names[-1] == ANY_PATTERN:
             node_names.pop()
+
+        query_part_regexps = []
+        for part in node_names:
+            query_part_regexps.append(re.compile(part))
         log.info("PATH: " + str(node_names))
         current_branches = [self.api.branch()]
         depth = 0
         leaves = []
-        for node_name in node_names:
-            log.info("Node: " + str(node_name))
+        for node_regexp in query_part_regexps:
+            log.info("Node: " + str(node_regexp))
             log.info("Cur branch: " + str(current_branches))
             next_branch_ids = []
             depth += 1
@@ -38,10 +60,11 @@ class OpenTSDBMetricsMeta(object):
             leaves = []
             for current_branch in current_branches:
                 for branch in current_branch['branches'] or []:
-                    if node_name == '*' or self._branch_current_node_name(branch) == node_name:
+                    if node_regexp.match(self._branch_current_node_name(branch)):
                         next_branch_ids.append(branch['branchId'])
                 for leaf in current_branch['leaves'] or []:
-                    leaves.append(leaf)
+                    if node_regexp.match(leaf['displayName']):
+                        leaves.append(leaf)
             log.info("next_branch_ids: " + str(next_branch_ids))
 
             current_branches = []
@@ -55,8 +78,7 @@ class OpenTSDBMetricsMeta(object):
             for branch in current_branch['branches'] or []:
                 result.append(
                     {
-                        "metric_path": ".".join(
-                            node_names + [self._branch_current_node_name(branch)]),
+                        "metric_path": self._branch_current_path(branch),
                         "isLeaf": False
                     }
                 )
@@ -69,9 +91,6 @@ class OpenTSDBMetricsMeta(object):
                 )
         for leaf in leaves:
             log.info("LEAF: " + str(leaf))
-            last_token = path.rsplit('.', 1)[-1]
-            if last_token and leaf['displayName'] != last_token:
-                continue
             result.append(
                 {
                     "metric_path": leaf["metric"],
